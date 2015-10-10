3f05bae114b9eaa5f490a00126719ed277d86ec1
Michael Chen <chen.3221@osu.edu>
2015-10-10 12:10:08 -0400

    Added Camera

--- app/src/main/java/rick/khe2015/MapsDisplay.java
+++ app/src/main/java/rick/khe2015/MapsDisplay.java
@@ -6,6 +6,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -100,11 +101,30 @@ public class MapsDisplay extends AppCompatActivity {
         }
     });*/
 
+    /*
+        Action bar created
+     */
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu items for use in the action bar
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_activity_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    /*
+        Method for when something on the action bar is selected
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_camera:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
